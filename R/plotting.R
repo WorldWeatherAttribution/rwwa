5b14f114ccba26e26a5204d0ff0bbfd74ca07a0a
Clair Barnes <clair.with.no.e@gmail.com>
2024-04-22 11:38:36 +0100

    Fixed erroneous reference to df in plot_covtrend

--- R/plotting.R
+++ R/plotting.R
@@ -79,7 +79,7 @@ plot_covtrend <- function(mdl, xcov, plot_cov = NA, ci_cov = NA, ev, ev_x = NA, 
 
   if(is.na(unlist(plot_cov)[1])) {
     # if no plotting covariate provided, fix all covariates at mean value except for xcov
-    plot_cov <- data.frame(sapply(mdl$covnm, function(cnm) if(cnm == xcov) {df[,xcov]} else {mean(df[,cnm])}, simplify = F))
+    plot_cov <- data.frame(sapply(mdl$covnm, function(cnm) if(cnm == xcov) {mdl$data[,xcov]} else {mean(mdl$data[,cnm])}, simplify = F))
   }
 
   plot(x, mdl$x, pch = 20, main = main, xlab = "", ylab = "", ylim = ylim, xlim = xlims,
